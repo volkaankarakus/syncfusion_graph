40c5e119b0cdddeec10c5d2553a5eae5a6c3e23d
Volkan Karakus <vkarakus@etu.edu.tr>
2022-09-26 15:56:29 +0300

    Implement Pieseries

--- syncfusion_graph/lib/home_view/home_view.dart
+++ syncfusion_graph/lib/home_view/home_view.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -9,8 +9,61 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  late List<GDPData> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: SfCircularChart(
+          title: ChartTitle(
+            text: 'Test Chart',
+            alignment: ChartAlignment.center,
+          ),
+          legend: Legend(
+              isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+          tooltipBehavior: _tooltipBehavior,
+          series: <CircularSeries>[
+            PieSeries<GDPData, String>(
+              // or DoughnutSeries
+              dataSource: _chartData,
+              xValueMapper: (GDPData data, _) => data.continent,
+              yValueMapper: (GDPData data, _) => data.gdp,
+              enableTooltip: true,
+              dataLabelSettings: const DataLabelSettings(
+                isVisible: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData('Africa', 1600),
+      GDPData('USA', 2100),
+      GDPData('Germany', 1100),
+      GDPData('France', 1530),
+      GDPData('Switzerland', 3000),
+      GDPData('Italy', 2100),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  final String continent;
+  final int gdp;
+
+  GDPData(this.continent, this.gdp);
 }
