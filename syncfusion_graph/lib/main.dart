654772936e7180c4f324bbf2a8131fb9d1a91f11
Volkan Karakus <vkarakus@etu.edu.tr>
2022-09-27 14:04:58 +0300

    implements Users to calendar

--- syncfusion_graph/lib/main.dart
+++ syncfusion_graph/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_graph/calender_view/calender_view.dart';
+import 'package:syncfusion_graph/add_resource_to_calendar/add_resource_to_calendar_view.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: CalenderView(),
+      home: const AddResourceToCalendarView(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
     );
   }
 }
